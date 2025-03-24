fb0fe282dfeca224252285fc725a414f285e03bd
guilhermetuba <guilhermetuba@gmail.com>
2025-03-23 23:50:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const { google } = require('googleapis');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config();
 
 // Substitua com as credenciais da sua conta de serviço ou OAuth2
-const oAuth2Client = new google.auth.OAuth2(
-  'SEU_CLIENT_ID', 
-  'SEU_CLIENT_SECRET', 
-  'SEU_REDIRECT_URI'
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,        // Usando a variável de ambiente
+  process.env.CLIENT_SECRET,    // Usando a variável de ambiente
+  process.env.REDIRECT_URI     // Usando a variável de ambiente
 );
 
+const scopes = ['https://www.googleapis.com/auth/spreadsheets'];
+
+
 // Defina o token de autenticação previamente obtido (via OAuth2)
-oAuth2Client.setCredentials({ access_token: 'SEU_ACCESS_TOKEN' });
+oAuth2Client.setCredentials({ access_token: 'ya29.a0AeXRPp4M_Vg3VYc9uZ9JBVEu9Aof2qtrd_hHLjJNPCmL985IRoCSSxkI_qhkBwJhEfMZTkNI0lAO68Y0Wl2dAdf-IfpvDUKNo-dRB5AEoInx7LHymeMYuI2TOous0qJMwt18V_A11aHDm6ChvoTIuChbReYLf-QGdqX6KIM9aCgYKAYISARMSFQHGX2Mi_SmMInYWHR8TSP7rj8xQlg0175' });
 
 // Inicializa o cliente do Google Sheets
 const sheets = google.sheets({ version: 'v4', auth: oAuth2Client });
