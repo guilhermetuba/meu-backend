1357abad99f8dfc43767d5cf7c392b0b6dcc99ed
guilhermetuba <guilhermetuba@gmail.com>
2025-03-25 15:58:05 -0300

    Update cadastro_produto.js

--- api/cadastro_produto.js
+++ api/cadastro_produto.js
@@ -1,4 +1,13 @@
-  if (req.method === "POST") {
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  
+if (req.method === "POST") {
     try {
       const { codigo, produto, fornecedor, categoria, quantidade, precoCusto, precoVenda } = req.body;
 
