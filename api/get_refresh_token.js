1222e12b6f4d815410de3a0f1593b7d4f9c2990c
guilhermetuba <guilhermetuba@gmail.com>
2025-03-24 23:26:57 -0300

    Update get_refresh_token.js

--- api/get_refresh_token.js
+++ api/get_refresh_token.js
@@ -1,14 +1,14 @@
-const { google } = require('googleapis');
-
-const oauth2Client = new google.auth.OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  process.env.REDIRECT_URI
-);
-
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+import { google } from 'googleapis';
 
 export default function handler(req, res) {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URI
+  );
+
+  const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+
   const authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
