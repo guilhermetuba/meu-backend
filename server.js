ce62f1f6e6868b532f238d76ac5a55e4b4b3fc26
guilhermetuba <guilhermetuba@gmail.com>
2025-03-24 23:34:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 3000;
 
 // Configuração OAuth2
 const oauth2Client = new google.auth.OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  process.env.REDIRECT_URI
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
 );
 
 // Define o Refresh Token para renovar o Access Token
