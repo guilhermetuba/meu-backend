379dfa77b98eb2cc96a4b53c5a8316dbec8ac3a9
guilhermetuba <guilhermetuba@gmail.com>
2025-03-27 20:34:59 -0300

    Update [cpf].js

--- api/atualiza_cliente/[cpf].js
+++ api/atualiza_cliente/[cpf].js
@@ -100,3 +100,17 @@ export default async function handler(req, res) {
 
   return res.status(405).json({ message: 'Método não permitido' });
 }
+
+async function authenticate() {
+  const { google } = require('googleapis');
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+  );
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+
+  return google.sheets({ version: 'v4', auth: oauth2Client });
+}
