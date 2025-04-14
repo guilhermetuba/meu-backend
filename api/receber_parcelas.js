5598ab968c0773ee7d303d81c6feff742007487c
guilhermetuba <guilhermetuba@gmail.com>
2025-04-13 23:02:47 -0300

    Update receber_parcelas.js

--- api/receber_parcelas.js
+++ api/receber_parcelas.js
@@ -84,3 +84,19 @@ export default async function handler(req, res) {
     }
   }
 }
+
+async function authenticate() {
+  const { google } = require("googleapis");
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+
+  const sheets = google.sheets({ version: "v4", auth: oauth2Client });
+  return sheets;
+}
