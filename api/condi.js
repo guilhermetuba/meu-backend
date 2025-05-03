235938ad54fe8781f43698ea33323c499a230866
guilhermetuba <guilhermetuba@gmail.com>
2025-05-03 16:06:05 -0300

    Update condi.js

--- api/condi.js
+++ api/condi.js
@@ -60,6 +60,10 @@ module.exports = async function handler(req, res) {
               const produtoEstoque = estoqueRows.find((erow, j) => j > 0 && erow[codigoEstoqueIndex] === codigo);
               const nomeProduto = produtoEstoque ? produtoEstoque[nomeProdutoIndex] : '';
 
+              console.log('Buscando produto:', codigo);
+              console.log('Produto encontrado:', produtoEstoque);
+              console.log('Nome do produto:', nomeProduto);
+
               produtos.push({
                 id: `${cpf}_${codigo}_${i}`,
                 data,
